62852526e81db8c434b1cdfe2f8f20c8dc504043
Mahesh Mahajan <mahajanmahesh935@gmail.com>
2024-06-20 17:42:04 +0530

    filter issue on contentList page

--- packages/nulp_elite/src/pages/search/ContentList.js
+++ packages/nulp_elite/src/pages/search/ContentList.js
@@ -419,12 +419,12 @@ const ContentList = (props) => {
             <Box className="h3-custom-title">
               {t("YOU_ARE_VIEWING_CONTENTS_FOR")}
             </Box>
-            {domainName && (
+            {domain && (
               <Box
                 sx={{ fontSize: "16px", fontWeight: "600", paddingLeft: "5px" }}
                 className="text-blueShade2 h4-custom"
               >
-                {domainName}
+                {domain}
               </Box>
             )}
           </Box>
