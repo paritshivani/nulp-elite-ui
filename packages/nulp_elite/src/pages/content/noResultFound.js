e88cc81933ad9e48737f52f1329637d5aef648b9
Ekta Yadav <ekta_y@tekditechnologies.com>
2024-07-24 23:12:56 +0530

    ui

--- packages/nulp_elite/src/pages/content/noResultFound.js
+++ packages/nulp_elite/src/pages/content/noResultFound.js
@@ -19,9 +19,8 @@ const NoResult = () => {
       <Box
         style={{
           fontSize: "30px",
-          border: "solid 1px #edece9",
           textAlign: "center",
-          margin: "20px 0 60px 0",
+          margin: "0 21px",
           BorderRadius: "10px",
         }}
         className="center-box"
