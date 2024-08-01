2106e0e420c6c0b87b39c7dc9a73fbc58baa3c95
sonaligarud <sonali.garud@tekditechnologies.com>
2024-08-01 16:02:11 +0530

    removed text from poll

--- packages/nulp_elite/src/pages/voting/votingDashboard.js
+++ packages/nulp_elite/src/pages/voting/votingDashboard.js
@@ -932,9 +932,6 @@ const votingDashboard = () => {
                 <Box className="h1-title fw-600 lg-mt-20">
                   {signlePOll.title}
                 </Box>
-                <Box className="lg-mt-12 h6-title Link">
-                  #CheerforBharat Paris Olympics Survey
-                </Box>
                 <Box>
                   <Box className="mt-9 h5-title">
                     Poll Created On:
